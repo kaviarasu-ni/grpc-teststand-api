6a584afa13af3d54457af210254b53ccf54e61d2
kaviarasu.selvarasu <kaviarasu.selvarasu@ni.com>
2023-11-07 19:06:37 +0530

    feat: added pipe support

--- SLClient/SLClient/Program.cs
+++ SLClient/SLClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NationalInstruments.TestStand.Grpc.Client.Utilities;
+using System.IO.Pipes;
 
 namespace SLClient
 {
@@ -20,8 +21,9 @@ namespace SLClient
             ClientOptions options = clientConfiguration.Options;
 
             var example = new Example(options);
-            example.OnConnectButtonClick(null, null);
-            example.OnRunSequenceFileButtonClick(null, null);
+            //example.OnConnectButtonClick(null, null);
+            //example.OnRunSequenceFileButtonClick(null, null);
+            CreateNamedPipe(example);
             var parser = new FileParser(
                 "C:\\ProgramData\\National Instruments\\Skyline\\TestPlan\\TestSequences\\INI\\TestFile.ini",
                 example);
@@ -32,6 +34,52 @@ namespace SLClient
         private static string GetConfigFilePathFromCommandLineIfSpecified(string[] args)
         {
             return args.Length == 1 ? args[0] : null;
+        }
+
+        private static async Task CreateNamedPipe(Example example)
+        {
+            Console.WriteLine("Creating Pipe....");
+            while (true)
+            {
+                using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("SLClientPipe", PipeDirection.InOut))
+                {
+                    pipeServer.WaitForConnection();
+
+                    using (StreamReader reader = new StreamReader(pipeServer))
+                    {
+
+                        try
+                        {
+                            // Read data from the named pipe
+                            string message = reader.ReadLine();
+
+                            Dictionary<string, Action> actions = new Dictionary<string, Action>()
+                            {
+                                { "Start" , () => example.OnRunSequenceFileButtonClick(null, null) },
+                                { "Resume" ,() => example.OnResumeButtonClick(null, null) },
+                                { "Pause" , ()=> example.OnBreakButtonClick(null, null)},
+                                { "Terminate" , ()=>example.OnTerminateButtonClick(null, null)},
+                            };
+
+                            Console.WriteLine("Pipe Message : {0}", message);
+                            if (actions.ContainsKey(message))
+                            {
+                                actions[message].Invoke();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid pipe message");
+                            }
+
+                        }
+                        catch (IOException ex)
+                        {
+                            // Handle IOException (pipe is broken or closed)
+                            Console.WriteLine("Error reading from the named pipe: " + ex.Message);
+                        }
+                    }
+                }
+            }
         }
     }
 }
